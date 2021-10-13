07ffcfb95a72092d56ad35f207763b5b33c55c15
dohungthinhSTS <41779725+dohungthinhSTS@users.noreply.github.com>
2021-10-13 08:58:26 +0700

    Update web_dav_requests_controller.rb

--- lib/calligraphy/rails/web_dav_requests_controller.rb
+++ lib/calligraphy/rails/web_dav_requests_controller.rb
@@ -4,15 +4,12 @@ module Calligraphy
   module Rails
     # Controller for all WebDAV requests.
     class WebDavRequestsController < ActionController::Base
-      protect_from_forgery with: :null_session
-
       include Calligraphy::Rails::WebDavMethods
       include Calligraphy::Rails::WebDavPreconditions
 
-#       before_action :verify_resource_scope
+      before_action :verify_resource_scope
       before_action :authenticate_with_digest_authentiation
       before_action :set_resource
-      skip_before_action :verify_authenticity_token
 
 
       # Entry-point for all WebDAV requests. Handles checking and validating
